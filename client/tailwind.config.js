faacc8252443fbbd157be3634bf4784987bbe3fd
Dharmendra Verma <its.dharmendra.v@gmail.com>
2025-12-03 01:41:39 +0530

    flowbite-error-fixed-final-2

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,13 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite-react/lib/esm/**/*.js",
-    "./node_modules/flowbite/**/*.js"    // ← MISSING (NEEDED)
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("flowbite/plugin"),
   ],
 }
